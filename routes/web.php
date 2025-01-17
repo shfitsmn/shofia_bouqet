c123228a8180ad82871dd055505fb9df89f9f84c
shfitsmn <jalanpahlawan1a@gmail.com>
2025-01-17 23:19:10 +0700

    add layouts

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.landing.index');
 });
 
 Route::get('/dashboard', function () {
-    return 'Halo';
+    return 'Dashboard';
 });
