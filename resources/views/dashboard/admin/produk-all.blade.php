07a16905d00a2e25b726c7565bae95bb3053dac1
Ahda Firly Barori <51354643+adhfry@users.noreply.github.com>
2025-01-21 20:37:41 +0700

    update

--- resources/views/dashboard/admin/produk-all.blade.php
+++ resources/views/dashboard/admin/produk-all.blade.php
@@ -11,51 +11,57 @@
         <div>
             <a href="{{ route('produk.create') }}" class="btn btn-primary">Tambah Data</a>
         </div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Gambar</th>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Kategori</th>
-                    <th scope="col">Deskripsi</th>
-                    <th scope="col">Harga</th>
-                    <th scope="col">Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($produks as $produk)
-                <tr>
-                    <th scope="row">{{ $loop->iteration }}</th>
-                    <td>
-                        <img src="{{ asset('storage/'.$produk->gambar) }}" class="img-thumbnail" alt="...">
-
-                    </td>
-                    <td>
-                        <h6 class="fw-semibold mb-0">{{ ucwords($produk->nama) }}</h6>
-                    </td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                        <div class="row">
-                            <div class="col-2">
+        <div class="table-responsive">
+            <table class="table table-striped table-hover align-middle">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Gambar</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Kategori</th>
+                        <th scope="col">Deskripsi</th>
+                        <th scope="col">Harga</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($produks as $produk)
+                    <tr>
+                        <th scope="row">{{ $loop->iteration }}</th>
+                        <td>
+                            <img src="{{ asset('storage/'.$produk->gambar) }}" class="img-thumbnail"
+                                alt="{{ $produk->nama }}" style="max-width: 80px; height: auto;">
+                        </td>
+                        <td>
+                            <h6 class="fw-semibold mb-0">{{ ucwords($produk->nama) }}</h6>
+                        </td>
+                        <td>
+                            <span class="badge bg-primary">{{ ucwords($produk->kategori ?? 'Tidak Ada') }}</span>
+                        </td>
+                        <td>
+                            <p class="text-truncate" style="max-width: 150px;">{{ $produk->deskripsi ?? 'Tidak Ada
+                                Deskripsi' }}</p>
+                        </td>
+                        <td>
+                            <span class="fw-bold text-success">Rp{{ number_format($produk->harga, 0, ',', '.') }}</span>
+                        </td>
+                        <td>
+                            <div class="d-flex gap-2">
                                 <a href="{{ route('produk.edit', $produk->id) }}" class="btn btn-sm btn-warning">
                                     <i class="ti ti-pencil"></i>
                                 </a>
-                            </div>
-                            <div class="col-2">
-                                <a href="{{ route('produk.delete', $produk->id) }}" class="btn btn-sm btn-danger">
+                                <a href="{{ route('produk.delete', $produk->id) }}" class="btn btn-sm btn-danger"
+                                    onclick="return confirm('Yakin ingin menghapus produk ini?')">
                                     <i class="ti ti-trash-x"></i>
                                 </a>
-
                             </div>
-                        </div>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
     </div>
 
 </div>
--- routes/web.php
+++ routes/web.php
@@ -33,16 +33,16 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/logout', [LandingController::class, 'logout'])->name('logout');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get("/user/all", [UserController::class,'index'])->name('user.index');
-    Route::get("/user/create", [UserController::class,'create'])->name('user.create');
+    Route::get("/user/all", [UserController::class, 'index'])->name('user.index');
+    Route::get("/user/create", [UserController::class, 'create'])->name('user.create');
     Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
     Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
     Route::post('/user/update/{id}', [UserController::class, 'update'])->name('user.update');
     Route::get('/user/delete/{id}', [UserController::class, 'destroy'])->name('user.delete');
 
-    Route::get("/produk/all",[ProdukController::class, 'index'])->name('produk.index');
-    Route::get("/produk/create",[ProdukController::class, 'create'])->name('produk.create');
-    Route::post('/produk/store', [ProdukController::class,'store'])->name('produk.store'); 
+    Route::get("/produk/all", [ProdukController::class, 'index'])->name('produk.index');
+    Route::get("/produk/create", [ProdukController::class, 'create'])->name('produk.create');
+    Route::post('/produk/store', [ProdukController::class, 'store'])->name('produk.store');
     Route::get('/produk/edit/{id}', [ProdukController::class, 'edit'])->name('produk.edit');
     Route::post('/produk/update/{id}', [ProdukController::class, 'update'])->name('produk.update');
     Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy'])->name('produk.delete');
